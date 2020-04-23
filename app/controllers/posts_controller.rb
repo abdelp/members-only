518271049d387d9979003e0e8ffae9d9df0a74ab
abdelp <juniorperezpy@gmail.com>
2020-04-23 18:29:19 -0400

    Add devise gem and all of the structure for the User

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,22 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user!, only: %i[new create]
+  include PostsHelper
+
+  def index
+    @posts = Post.all.order("created_at DESC")
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+end
