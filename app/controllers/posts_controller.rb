40fcbc7331c47233b881523c1b7c84b6fab32882
abdelp <juniorperezpy@gmail.com>
2020-04-24 18:16:55 -0400

    Remove commented code

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,30 +1,22 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[show edit update destroy]
   before_action :authenticate_user!, only: %i[new create]
-  # before_action user_signed_in?, only: [:new, :create]
+
   include PostsHelper
 
-  # GET /posts
-  # GET /posts.json
   def index
     @posts = Post.all.order('created_at DESC')
     @post = Post.new
   end
 
-  # GET /posts/1
-  # GET /posts/1.json
   def show; end
 
-  # GET /posts/new
   def new
     @post = Post.new
   end
 
-  # GET /posts/1/edit
   def edit; end
 
-  # POST /posts
-  # POST /posts.json
   def create
     @post = current_user.posts.build(post_params)
 
@@ -39,8 +31,6 @@ class PostsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /posts/1
-  # PATCH/PUT /posts/1.json
   def update
     respond_to do |format|
       if @post.update(post_params)
@@ -53,8 +43,6 @@ class PostsController < ApplicationController
     end
   end
 
-  # DELETE /posts/1
-  # DELETE /posts/1.json
   def destroy
     @post.destroy
     respond_to do |format|
@@ -65,12 +53,10 @@ class PostsController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_post
     @post = Post.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def post_params
     params.require(:post).permit(:title, :body)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,4 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations' }
   resources :posts, only: %i[new create index]
   root to: 'posts#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
