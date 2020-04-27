17d4547b68ab0f7fd2d0013c9f778494d9bb569c
abdelp <juniorperezpy@gmail.com>
2020-04-27 10:28:16 -0400

    Add uniqueness to username

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :username, uniqueness: true
   has_many :posts, dependent: :destroy
 end
